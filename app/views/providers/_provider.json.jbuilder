908d5136434c0c31fe44982da1e0131379d9b6b1
tsekhanskaya <anutta2222@gmail.com>
2022-07-29 16:06:05 +0300

    Added Provider

---
+++ app/views/providers/_provider.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! provider, :id, :name, :phone, :address, :created_at, :updated_at
+json.url provider_url(provider, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_29_124915) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_29_130351) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_29_124915) do
     t.string "description"
     t.string "price"
     t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "providers", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
