7bfee01d8ca95c88be15f570efe465d3ada8ecc2
tsekhanskaya <anutta2222@gmail.com>
2022-08-02 18:51:49 +0300

    Added meta tags

--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -3,6 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe Brand, type: :model do
-  it {should validate_presence_of :title}
-  it {should validate_presence_of :description}
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :description }
 end
